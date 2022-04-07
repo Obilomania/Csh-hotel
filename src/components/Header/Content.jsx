cae6701cbc89c8889f464478808a67c159ba81dd
obilomania <iloanyaobinna@gmail.com>
2022-04-07 09:11:56 +0800

    landing page complete

--- src/components/Header/Content.jsx
+++ src/components/Header/Content.jsx
@@ -5,18 +5,21 @@ const Content = () => {
     return (
         <>
             <div className="header__content">
-                <div className="span__lines">
-                    <span></span>
-                    <span></span>
+                <div className="landing__writings">
+                    <div className="span__lines">
+                        <span></span>
+                        <span></span>
+                    </div>
+                    <div className="writeup">
+                        <h1>Welcome to SCH Hotel</h1>
+                        <h3>Home Away From Home</h3>
+                    </div>
+                    <div className="span__lines">
+                        <span></span>
+                        <span></span>
+                    </div>
                 </div>
-                <div className="writeup">
-                    <h1>Welcome to SCH Hotel</h1>
-                    <h3>Home Away From Home</h3>
-                </div>
-                <div className="span__lines">
-                    <span></span>
-                    <span></span>
-                </div>
+                <button>Contact Us</button>
             </div>
         </>
     )
